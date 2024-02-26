d83e639e9cd48528aceec6921b9d262ed9f7812c
Lenon Anthony <lenonanthony77@icloud.com>
2024-02-26 16:41:55 -0300

    retirando login

--- project-setup/src/App.tsx
+++ project-setup/src/App.tsx
@@ -1,24 +1,20 @@
 import './App.css';
-import { useState } from 'react';
+// import { useState } from 'react';
 import KanbanBoard from './components/KanbanBoard';
-import Login from './components/Login';
+// import Login from './components/Login';
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  // const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   // Esta função normalmente envolveria a verificação de credenciais contra um banco de dados
-  const handleLogin = () => {
+  // const handleLogin = () => {
     // Simula um login bem-sucedido
-    setIsAuthenticated(true);
-  };
+    //setIsAuthenticated(true);
+  //};
 
   return (
     <div className="App">
-      {isAuthenticated ? (
         <KanbanBoard />
-      ) : (
-        <Login onLogin={handleLogin} />
-      )}
     </div>
   );
 }
